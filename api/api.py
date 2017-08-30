fed9ea731271ba4ecb1cfecf8df3126706f0dee7
MohsanM <mahmood.mohsan@gmail.com>
2017-08-31 00:58:53 +0100

    api app

---
+++ api/api.py
@@ -0,0 +1,14 @@
+from flask import Flask 
+from flask.ext.restful import Api, Resource, reqparse 
+
+app = Flask(__name__)
+api = Api(app)
+
+class EchoApi(Resource):
+	def get(self, value):
+		return {'hello': value}
+
+api.add_resource(EchoApi, '/echo/<value>', endpoint = 'echo')
+
+if __name__ == '__main__':
+	app.run

a721335adfa1ca46c3182bd19316f81d6b4ddd42
mohsanm <mahmood.mohsan@gmail.com>
2017-08-31 00:27:57 +0000

    changed back

--- api/api.py
+++ api/api.py
@@ -1,12 +1,12 @@
 from flask import Flask 
-from flask_restful import Api, Resource, reqparse 
+from flask.ext.restful import Api, Resource, reqparse 
 
 app = Flask(__name__)
 api = Api(app)
 
 class EchoApi(Resource):
 	def get(self, value):
-		return {'hello': value}
+	    return {'hello': value}
 
 api.add_resource(EchoApi, '/echo/<value>', endpoint = 'echo')
 
